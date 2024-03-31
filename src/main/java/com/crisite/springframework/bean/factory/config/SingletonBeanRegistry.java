9bc49f7791e45d63d19ed8733cbe199c62c094e1
puff <844858692@qq.com>
2024-03-31 23:55:52 +0800

    增加SingletonBeanRegistry注释

--- src/main/java/com/crisite/springframework/bean/factory/config/SingletonBeanRegistry.java
+++ src/main/java/com/crisite/springframework/bean/factory/config/SingletonBeanRegistry.java
@@ -2,6 +2,10 @@ package com.crisite.springframework.bean.factory.config;
 
 /**
  * 单例注册表
+ * 事实上，SingletonBeanRegistry 和 BeanFactory 的职责略有重复
+ * SingletonBeanRegistry 负责单例 Bean 的注册和获取
+ * BeanFactory 也负责 Bean 的创建、配置、和管理，这其中当然也包含了单例 Bean
+ * SingletonBeanRegistry 在功能上是 BeanFactory 的子集...?
  *
  * @Author: Rao Sheng
  * @Date: 2024/3/30 16:11
